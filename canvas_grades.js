665c92bd2c540fe7d9bbeb22dc8687a77a653291
mnmzc <100777450+mnmzc@users.noreply.github.com>
2023-06-14 11:03:30 -0400

    Add more features to canvas_grades.js

--- canvas_grades.js
+++ canvas_grades.js
@@ -14,10 +14,26 @@ const conversions = {
     'F': [0, 64],
 }
 
+const ap_Offset = 1
+const hon_Offset = 0.5
+const rate = 0.1
+const offset = -55
+
+function insertAfter(referenceNode, newNode) {
+    referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
+}
+
+let item_sum = 0;
+let item_count = 0;
 for (let i = 0; i < percentElements.length; i++) {
     const element = percentElements.item(i);
+    const previous = element.previousElementSibling;
+    const dropdown = element.nextElementSibling.nextElementSibling;
     const txt = element.innerHTML.trim().replace(/^\D+/g, '');
     const num = parseFloat(txt);
+
+    const gpa_element = document.createElement('td');
+    gpa_element.innerHTML = ' GPA: N/A ';
 
     if (num.toString() != 'NaN') {
         const val = Math.round(num);
@@ -27,5 +43,36 @@ for (let i = 0; i < percentElements.length; i++) {
                 element.innerHTML = ` ${num}% (${grade}) `;
             }
         }
+
+        // Get Grade Modifiers
+        const prevTitle = previous.getElementsByTagName('a')[0].innerHTML
+        const isHon = prevTitle.indexOf('Hon') != -1 || prevTitle.indexOf('Honor') != -1 || prevTitle.indexOf('Honors') != -1;
+        const isAP = prevTitle.indexOf('AP') != -1;
+
+        // Get raw GPA
+        const rawGPA = (num + offset) * rate;
+        let gpa = rawGPA;
+
+        if (isAP) {
+            gpa += 1;
+        } else if (isHon) {
+            gpa += 0.5;
+        }
+
+        gpa_element.innerHTML = " GPA: " + gpa.toFixed(2) + " "
+
+        // is Half Year Class
+        const isHalfYear = dropdown.getElementsByTagName('select')[0].childElementCount <= 3;
+
+        // Average
+        item_sum += gpa;
+        item_count += isHalfYear && 0.5 || 1
     }
+
+    insertAfter(element, gpa_element)
 }
+
+const predictedGPA = document.createElement('h4');
+predictedGPA.innerHTML = ' Predicted GPA: ' + (item_sum / item_count).toFixed(2) + ' ';
+
+insertAfter(document.getElementsByTagName('h2')[0], predictedGPA)
